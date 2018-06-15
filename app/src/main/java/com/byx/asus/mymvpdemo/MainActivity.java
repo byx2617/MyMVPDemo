7da3b21d3316b615b88e0d584ad03825db41f8dc
John Doe <l>
2018-06-15 14:21:55 +0800

    第三次提交   改动了mainactivity 和布局

--- app/src/main/java/com/byx/asus/mymvpdemo/MainActivity.java
+++ app/src/main/java/com/byx/asus/mymvpdemo/MainActivity.java
@@ -18,6 +18,6 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
 
         tv = (TextView) findViewById(R.id.tv);
-        tv.setText("");
+        tv.setText("天下都是本尊的！！");
     }
 }
